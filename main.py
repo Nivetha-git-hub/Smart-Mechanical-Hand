3b8580f2c6102cb777f6b505e256285991ff3252
Nivetha-git-hub <94275196+Nivetha-git-hub@users.noreply.github.com>
2023-04-03 19:23:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,46 +2,46 @@ from cvzone.HandTrackingModule
 import HandDetector
 import cv2
 
-cap = cv2.VideoCapture('http://192.168.10.58:8080/video')
+cap = cv2.VideoCapture(0)
  
 detector = HandDetector(detectionCon=0.8, maxHands=2)
 #mySerial= cvzone.SerialObject("COM3",9600,1)
 while True:
-    # Get image frame
+   
     success, img1 = cap.read()
     img = cv2.resize(img1,(600,400))
     
-    # Find the hand and its landmarks
-    hands, img = detector.findHands(img)  # with draw
-    # hands = detector.findHands(img, draw=False)  # without draw
+    
+    hands, img = detector.findHands(img)  
+    # hands = detector.findHands(img, draw=False)  
 
     if hands:
-        # Hand 1
+        
         hand1 = hands[0]
-        lmList1 = hand1["lmList"]  # List of 21 Landmark points
-        bbox1 = hand1["bbox"]  # Bounding box info x,y,w,h
-        centerPoint1 = hand1['center']  # center of the hand cx,cy
-        handType1 = hand1["type"]  # Handtype Left or Right
+        lmList1 = hand1["lmList"]  
+        bbox1 = hand1["bbox"]  
+        centerPoint1 = hand1['center']  
+        handType1 = hand1["type"] 
 
         fingers1 = detector.fingersUp(hand1)
         print(fingers1)
-        #mySerial.sendData(fingers1)
+        
 
         if len(hands) == 2:
             # Hand 2
             hand2 = hands[1]
-            lmList2 = hand2["lmList"]  # List of 21 Landmark points
-            bbox2 = hand2["bbox"]  # Bounding box info x,y,w,h
-            centerPoint2 = hand2['center']  # center of the hand cx,cy
-            handType2 = hand2["type"]  # Hand Type "Left" or "Right"
+            lmList2 = hand2["lmList"]  
+            bbox2 = hand2["bbox"] 
+            centerPoint2 = hand2['center'] 
+            handType2 = hand2["type"]  
 
             fingers2 = detector.fingersUp(hand2)
             print(fingers2)
 
-            # Find Distance between two Landmarks. Could be same hand or different hands
-            length, info, img = detector.findDistance(lmList1[8], lmList2[8], img)  # with draw
-            # length, info = detector.findDistance(lmList1[8], lmList2[8])  # with draw
-    # Display
+            
+            length, info, img = detector.findDistance(lmList1[8], lmList2[8], img)  
+            # length, info = detector.findDistance(lmList1[8], lmList2[8])  
+   
     cv2.imshow("Image", img)
     cv2.waitKey(1)
 cap.release()
@@ -53,6 +53,6 @@ cv2.destroyAllWindows()
 # # Convert the model.
 # converter = tf. lite. TFLiteConverter. from_saved_model(fruits_dir)
 # tflite_model = converter. convert()
-# # Save the model.
+
 # with open('cv_model.tflite', 'wb') as f:
 #     f.write(tflite_model)
